cb8ad13afe1b0096fc700ed8e07055c7eb77dfd7
flynsarmy <flynsarmy@gmail.com>
2015-01-30 10:24:32 +1000

    first commit

---
+++ Plugin.php
@@ -0,0 +1,31 @@
+<?php namespace Flynsarmy\IdeHelper;
+
+use App;
+use System\Classes\PluginBase;
+
+/**
+ * IdeHelper Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'IdeHelper',
+            'description' => 'No description provided yet...',
+            'author'      => 'Flynsarmy',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    public function boot()
+    {
+        App::register('\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider');
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# IDE Helpers
+
+This plugin adds [barryvdh/ide-helpers](https://github.com/barryvdh/laravel-ide-helper) package to October for better IDE support.
+
+## Installation
+
+* `git clone` into */plugins/flynsarmy/idehelper*
+* `composer update`
+* Open */plugins/flynsarmy/idehelper/vendor/barryvdh/laravel-ide-helper/src/Generator.php* and around line 113 change `if (class_exists('Auth')) {` to `if (class_exists('Auth') && method_exists('Auth', 'driver')) {` (This is necessary until [PR#155](https://github.com/barryvdh/laravel-ide-helper/pull/155) is merged)
+* `php artisan ide-helper:generate`
